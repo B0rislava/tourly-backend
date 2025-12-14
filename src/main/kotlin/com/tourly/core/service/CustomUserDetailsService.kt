1ec8898643d152c9e7d675166e3f3e7d9cd4047f
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-12-14 16:47:28 +0200

    TOURLY-1 - Added CustomUserDetailsService

---
+++ src/main/kotlin/com/tourly/core/service/CustomUserDetailsService.kt
@@ -0,0 +1,21 @@
+package com.tourly.core.service
+
+import com.tourly.core.data.repository.UserRepository
+import com.tourly.core.security.CustomUserDetails
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByEmail(username)
+            .orElseThrow { UsernameNotFoundException("User not found with email: $username") }
+
+        return CustomUserDetails(user)
+    }
+}
