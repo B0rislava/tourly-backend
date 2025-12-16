def194698c0bd5ce071cfe6f0fa3030810faa1e1
B0rislava <borislava.g.ivanova.2021@elsys-bg.org>
2025-12-16 19:53:00 +0200

    TOURLY-1 - Updated the jwt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("io.jsonwebtoken:jjwt-api:0.13.0")
 
 	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
